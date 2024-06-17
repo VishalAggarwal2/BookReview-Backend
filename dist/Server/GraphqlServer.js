168068a3b2cdafb6535c9608b4a7ff85299ee74e
VishalAggarwal2 <136411362+VishalAggarwal2@users.noreply.github.com>
2024-06-17 14:53:00 +0530

    completed almost backend

--- dist/Server/GraphqlServer.js
+++ dist/Server/GraphqlServer.js
@@ -12,11 +12,32 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const GraphqlServer_1 = __importDefault(require("./Server/GraphqlServer"));
-const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
-    const app = yield (0, GraphqlServer_1.default)();
-    app.listen(8000, () => {
-        console.log("Hello From The Typescript ");
+const server_1 = require("@apollo/server");
+const express4_1 = require("@apollo/server/express4");
+const express_1 = __importDefault(require("express"));
+const initiateServe = () => __awaiter(void 0, void 0, void 0, function* () {
+    const app = (0, express_1.default)();
+    app.use(express_1.default.json());
+    const typeDefs = `
+        type Query {
+            hello: String
+        }
+    `;
+    const resolvers = {
+        Query: {
+            hello: () => "Hello from the backend!"
+        }
+    };
+    const server = new server_1.ApolloServer({
+        typeDefs,
+        resolvers,
     });
+    yield server.start();
+    const middleware = (0, express4_1.expressMiddleware)(server);
+    app.use("/graphql", middleware);
+    app.get("/", (req, res) => {
+        res.send("hello");
+    });
+    return app;
 });
-startServer();
+exports.default = initiateServe;
